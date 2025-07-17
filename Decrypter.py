2c8bc424d56943841b3767b9c143fa3198bcbaf4
rtwaerwaewa <152304922+rtwaerwaewa@users.noreply.github.com>
2025-07-17 14:24:42 +0300

    Create Decrypter.py

---
+++ Decrypter.py
@@ -0,0 +1,67 @@
+import cryptography
+import cryptography.fernet
+import platform
+import GPUtil
+import os
+import sys
+import threading
+from pathlib import Path
+
+def masaustu_yolu() -> str:
+    masaustu = Path.home() / "Desktop"
+    if masaustu.exists():
+        return masaustu
+    else:
+        return None
+
+def dosya_decrypt(klasor_yolu: str, key: bytes) -> list:
+    file_list = []
+    dosyaklasor_list = os.walk(klasor_yolu)
+    for root, dirs, files in dosyaklasor_list:
+        for file in files:
+            full_path = os.path.join(root, file)
+            file_list.append(full_path)
+
+    i = 0
+    while i < len(file_list):
+        with open(file_list[i], 'rb') as okunan_dosya:
+            icerik = okunan_dosya.read()
+        cozulmus_icerik = cryptography.fernet.Fernet(key).decrypt(icerik)
+        with open(file_list[i], 'wb') as yazilan_dosya:
+            yazilan_dosya.write(cozulmus_icerik)
+        print("[*] Dosya çözüldü! : {}".format(file_list[i]))
+        i = i + 1
+
+def gpname() -> str:
+    gpus = GPUtil.getGPUs()
+    for gpu in gpus:
+        return gpu.name
+
+if __name__ == '__main__':
+    if platform.system().lower() == "windows": os.system("cls")
+    else: os.system("clear")
+  
+    platform.gpname = gpname
+  
+    print(f"[^] OS : {platform.system()}")
+    print(f"[^] CPU : {platform.processor()}")
+    print(f"[^] GPU : {platform.gpname()}")
+    key = input("[-] Anahtarı Giriniz: ").encode("utf-8")
+    print("\n########################################################################")
+    suanki_klasor = os.getcwd()
+
+    thread_1 = threading.Thread(target=dosya_decrypt, args=(suanki_klasor, key,))
+    thread_2 = threading.Thread(target=dosya_decrypt, args=(masaustu_yolu, key,))
+  
+    thread_1.daemon = False
+    thread_2.daemon = False
+  
+    thread_1.start()
+    thread_2.start()
+  
+    thread_1.join()
+    thread_2.join()
+
+    print("İşlem sona erdi.")
+
+    sys.exit(0)
